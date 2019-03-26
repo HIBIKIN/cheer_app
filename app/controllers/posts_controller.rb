86bea1402a940b701ffd4ef78d68b57e18e19184
HIBIKIN <hi3bi1ki5@gmail.com>
2019-03-26 13:21:35 +0900

    create post and devise bi

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post, notice: "投稿しました。"
+  end
+
+  def show
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:body)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
